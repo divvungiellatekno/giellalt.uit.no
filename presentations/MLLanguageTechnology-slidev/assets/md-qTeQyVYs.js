9038f76c5a46a80f0e0cda99d4333917bd330749
snomos <snomos@users.noreply.github.com>
2025-11-27 13:43:18 +0000

    deploy: 08daa6067f1f1f716a565686a858fcbbf44ba559

---
+++ presentations/MLLanguageTechnology-slidev/assets/md-qTeQyVYs.js
@@ -0,0 +1,19 @@
+import{_ as o}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BcodYZXe.js";import{b as r,o as c,w as l,g as s,e as u,m as f,ad as n,v as m,x as d,T as e}from"./modules/vue-RVvBq83K.js";import{I as g}from"./slidev/default-qva_BKeJ.js";import{u as k,f as x}from"./slidev/context-wrh3yJeR.js";import"./modules/unplugin-icons-ClLoun1q.js";import"./index-CLuFRfFK.js";import"./modules/shiki-BocPJKSR.js";const y={__name:"slides.md__slidev_6",setup(P){const{$clicksContext:i,$frontmatter:t}=k();return i.setup(),(_,a)=>{const p=o;return c(),r(g,m(d(e(x)(e(t),5))),{default:l(()=>[a[1]||(a[1]=s("h3",null,"Example — lexc",-1)),a[2]||(a[2]=s("p",null,"A very simplistic English lexicon:",-1)),u(p,f({},{title:"",ranges:[]}),{default:l(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"Multichar_Symbols")]),n(`
+`),s("span",{class:"line"},[s("span",null,"+V +Pres +3Sg +PresPtc +Past")]),n(`
+`),s("span",{class:"line"},[s("span")]),n(`
+`),s("span",{class:"line"},[s("span",null,"###  Lexicon containing lexical stems:")]),n(`
+`),s("span",{class:"line"},[s("span",null,"LEXICON Root")]),n(`
+`),s("span",{class:"line"},[s("span",null," walk V ;")]),n(`
+`),s("span",{class:"line"},[s("span",null," talk V ;")]),n(`
+`),s("span",{class:"line"},[s("span",null," pack V ;")]),n(`
+`),s("span",{class:"line"},[s("span")]),n(`
+`),s("span",{class:"line"},[s("span",null,"###  Lexicon containing POS tag only:")]),n(`
+`),s("span",{class:"line"},[s("span",null,"LEXICON V")]),n(`
+`),s("span",{class:"line"},[s("span",null," +V: V-suff ;")]),n(`
+`),s("span",{class:"line"},[s("span")]),n(`
+`),s("span",{class:"line"},[s("span",null,"###  Lexicon containing inflectional suffixes and corresponding tags:")]),n(`
+`),s("span",{class:"line"},[s("span",null,"LEXICON V-suff")]),n(`
+`),s("span",{class:"line"},[s("span",null," +Pres+3Sg:s   # ;")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     +Past:ed  # ;")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  +PresPtc:ing # ;")]),n(`
+`),s("span",{class:"line"},[s("span",null,"     +Pres:    # ;")])])],-1)])]),_:1},16)]),_:1},16)}}};export{y as default};
