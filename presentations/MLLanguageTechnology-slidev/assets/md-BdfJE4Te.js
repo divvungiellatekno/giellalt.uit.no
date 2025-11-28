1ab6c0e6c1b58597b66a091b4fd4bbe4f7fce1d8
snomos <snomos@users.noreply.github.com>
2025-11-28 12:22:33 +0000

    deploy: 30b27c48c8ad658a97bd261f7bb8a4ab9b3883a1

---
+++ presentations/MLLanguageTechnology-slidev/assets/md-BdfJE4Te.js
@@ -0,0 +1,15 @@
+import{_ as p}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DjJ9cDs3.js";import{b as r,o as u,w as a,g as s,e as m,m as d,ad as n,v as f,x as c,T as e}from"./modules/vue-RVvBq83K.js";import{I as k}from"./slidev/default-CXug-p2P.js";import{u as g,f as b}from"./slidev/context-BII5ojYb.js";import"./modules/unplugin-icons-ClLoun1q.js";import"./index-g_eUet94.js";import"./modules/shiki-BocPJKSR.js";const T={__name:"slides.md__slidev_16",setup(y){const{$clicksContext:t,$frontmatter:i}=g();return t.setup(),(h,l)=>{const o=p;return u(),r(k,f(c(e(b)(e(i),15))),{default:a(()=>[l[1]||(l[1]=s("h3",null,"Layout definition",-1)),l[2]||(l[2]=s("p",null,"The layout definition is done using yaml syntax, and is just the following:",-1)),m(o,d({},{title:"",ranges:[]}),{default:a(()=>[...l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"modes:")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  mobile-default: |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    á š e r t y u i o p â")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    a s d f g h j k l ä đ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"      ž z č c v b n m ŋ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  mobile-shift: |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    Á Š E R T Y U I O P Â")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    A S D F G H J K L Ä Đ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"      Ž Z Č C V B N M Ŋ")]),n(`
+`),s("span",{class:"line"},[s("span")]),n(`
+`),s("span",{class:"line"},[s("span",null,"longpress:")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  Á: Q")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  A: Å   Á À Â Ã Ạ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  á: q")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  a: å   á à â ã ạ")])])],-1)])]),_:1},16),l[3]||(l[3]=s("ul",null,[s("li",null,[s("a",{href:"https://github.com/giellalt/keyboard-sms/blob/main/sms.kbdgen/layouts/sms.yaml",target:"_blank"},"Skolt Sami desktop keyboard")])],-1)),l[4]||(l[4]=s("p",null,"(Demo: Skolt Sámi macOS keyboard)",-1))]),_:1},16)}}};export{T as default};
